f70dccf928f85e2eafaf0c8ee044867376c67da2
henriquegreve <xb9gx5zs1>
2022-01-19 15:49:27 -0300

    Adicionando Lombok / Refatoracao nas classes

--- src/main/java/com/greve/minhasfinancas/model/entity/Usuario.java
+++ src/main/java/com/greve/minhasfinancas/model/entity/Usuario.java
@@ -1,10 +1,18 @@
 package com.greve.minhasfinancas.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table( name = "usuario", schema = "financas")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
     @Id
@@ -20,62 +28,4 @@ public class Usuario {
 
     @Column(name = "senha")
     private String senha;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return Objects.equals(id, usuario.id)
-                && Objects.equals(nome, usuario.nome)
-                && Objects.equals(email, usuario.email)
-                && Objects.equals(senha, usuario.senha);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, email, senha);
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                ", senha='" + senha + '\'' +
-                '}';
-    }
 }
