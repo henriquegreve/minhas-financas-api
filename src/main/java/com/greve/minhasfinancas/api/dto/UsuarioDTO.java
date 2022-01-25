860b03085fcc41e0c8b31b287282527b08068251
henriquegreve <xb9gx5zs1>
2022-01-25 17:04:28 -0300

    Implementado endpoint de autenticacao usuario

---
+++ src/main/java/com/greve/minhasfinancas/api/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.greve.minhasfinancas.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class UsuarioDTO {
+
+    private String nome;
+    private String email;
+    private String senha;
+}
---
+++ src/main/java/com/greve/minhasfinancas/api/resource/UsuarioResource.java
@@ -0,0 +1,51 @@
+package com.greve.minhasfinancas.api.resource;
+
+import com.greve.minhasfinancas.api.dto.UsuarioDTO;
+import com.greve.minhasfinancas.exception.ErroAutenticacao;
+import com.greve.minhasfinancas.exception.RegraNegocioException;
+import com.greve.minhasfinancas.model.entity.Usuario;
+import com.greve.minhasfinancas.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+
+    private UsuarioService service;
+
+    public UsuarioResource(UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/autenticar")
+    public ResponseEntity autenticar( @RequestBody UsuarioDTO dto ) {
+        try {
+            Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+            return ResponseEntity.ok(usuarioAutenticado);
+        } catch (ErroAutenticacao e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+        Usuario usuario = Usuario.builder()
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .senha(dto.getSenha())
+                .build();
+
+        try {
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
