bbb2c1307d6ad6d400577ebabcc773559cd35229
henriquegreve <xb9gx5zs1>
2022-01-26 00:05:15 -0300

    Concluindo o Controller de Lancamentos

--- src/main/java/com/greve/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/greve/minhasfinancas/model/repository/LancamentoRepository.java
@@ -1,6 +1,7 @@
 package com.greve.minhasfinancas.model.repository;
 
 import com.greve.minhasfinancas.model.entity.Lancamento;
+import com.greve.minhasfinancas.model.enums.TipoLancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,7 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
     @Query(value =
             " select sum(l.valor) from Lancamento l join l.usuario u "
                     + "where u.id = :idUsuario and l.tipo = :tipo group by u ")
-    BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") String tipo);
-
+    BigDecimal obterSaldoPorTipoLancamentoEUsuario(
+            @Param("idUsuario") Long idUsuario,
+            @Param("tipo") TipoLancamento tipo);
 }
