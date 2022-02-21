69e538577797ea73f2e67ea59c2bc039c84dc849
henriquegreve <xb9gx5zs1>
2022-02-21 16:53:55 -0300

    Correcoes

--- src/main/java/com/greve/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/greve/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,12 +1,12 @@
 package com.greve.minhasfinancas.api.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
 
     private String nome;
